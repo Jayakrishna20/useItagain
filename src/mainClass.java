679d98a007e68036ab73a24f4260858b351732e8
joyalfrancis23 <joyalfrancis23@gmail.com>
2022-02-20 21:44:51 +0530

    SignUp page setup

---
+++ src/mainClass.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author joyal
+ */
+public class mainClass {
+    public static void main(String args[]){
+        System.out.print("HEllo World!!");
+    }
+}
